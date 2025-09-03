e4c43b31d31c92e9c801fdd336e4b020776ba70b
andrelbarbosas <andrelbarbosas@oulook.com>
2025-09-02 22:13:30 -0300

    https://github.com/andrelbarbosas/1TIAPR.git

---
+++ Aula1/EX2.py
@@ -0,0 +1,9 @@
+Nome = 'André'
+Idade = '30'
+Altura = '1.78'
+Cidade = 'Santo André'
+Sexo = True
+
+
+print(f"")
+
---
+++ Aula1/Ex3.py
@@ -0,0 +1,9 @@
+a = 10
+b = 20
+c = 0
+
+a = b
+b = a
+
+print("var A: ", a)
+print("var B: ", b)
